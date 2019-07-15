ba04992aef316c0d37df4805845421a6fcb43fbf
Recardo Poole <recardo.poole@gmail.com>
2019-07-14 21:38:00 -0300

    bd call setup

---
+++ js/db.js
@@ -0,0 +1,29 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const dbname = 'crud_todo';
+
+const url = 'mongodb://localhost:27017';
+const mpngoOptions = {
+  userNewUrlParser: true
+};
+
+
+const state = {
+  db = null
+};
+
+const connect = (cd) => {
+  if (state.db)
+    cd();
+  else(
+    MongoClient.connect(url, mongoOptions, (err, client) => {
+      if (err)
+        cd(err);
+      else {
+        state.db = client.db(dbname);
+        cd();
+      }
+    });
+  )
+}
+

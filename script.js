042f50e858525adf138bcd1a0ccaee7678294b94
Recardo Poole <recardo.poole@gmail.com>
2019-07-22 23:22:30 -0600

    Finished CRUD App

--- script.js
+++ script.js
@@ -1,143 +1,142 @@
 /*jshint esversion: 6 */
 
 $(document).ready(() => {
-
-  const display = $('#display');
-  const form = $('#form');
-  const todoUserInput = $('#todoUserInput');
+  const display = $("#display");
+  const form = $("#form");
+  const todoUserInput = $("#todoUserInput");
   const message = $("#message");
   message.hide();
-
-  const displayMessage = (flag,msg)=>{
+  const displayMessage = (flag, msg) => {
     // successful
-    if(flag){
-        message.removeClass('alert-danger');
-        message.addClass('alert-success');
-        message.html(msg);
-        message.show();
+    if (flag) {
+      message.removeClass("alert-danger");
+      message.addClass("alert-success");
+      message.html(msg);
+      message.show();
+    } else {
+      message.removeClass("alert-success");
+      message.addClass("alert-danger");
+      message.html(msg);
+      message.show();
     }
-    else{
-        message.removeClass('alert-success');
-        message.addClass('alert-danger');
-        message.html(msg);
-        message.show();
-    }
-};
+  };
 
-const getTodos = ()=>{
-    fetch('/getTodos',{method : "get"}).then((response)=>{
+  const getTodos = () => {
+    fetch("/getTodos", { method: "get" })
+      .then(response => {
         return response.json();
-    }).then((data)=>{
-        console.log(data);
+      })
+      .then(data => {
         displayTodos(data);
+        console.log(data);
+      });
+  };
+
+  getTodos();
+  const resetTodosInput = () => {
+    todoUserInput.val("");
+  };
+
+  const editTodo = (todo, todoID, editID) => {
+    let editBtn = $(`#${editID}`);
+    editBtn.click(() => {
+      fetch(`/${todo._id}`, {
+        method: "put",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({ todo: todoUserInput.val() })
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          if (data.ok == 1) {
+            let todoIndex = $(`#${todoID}`);
+            todoIndex.html(data.value.todo);
+            resetTodosInput();
+          }
+        });
     });
-};
+  };
 
-getTodos();
-
-const resetTodosInput = ()=>{
-  todoUserInput.val('');
-};
-
-const editTodo = (todo,todoID,editID)=>{
-  let editBtn = $(`#${editID}`);
-  editBtn.click(()=>{
-      fetch(`/${todo._id}`,{
-          method : "put",
-          headers : {
-              "Content-Type" : "application/json; charset=utf-8" 
-          },
-          body : JSON.stringify({todo : todoUserInput.val()})
-      }).then((response)=>{
+  const deleteTodo = (todo, listItemID, deleteID) => {
+    let deleteBtn = $(`#${deleteID}`);
+    deleteBtn.click(() => {
+      fetch(`/${todo._id}`, {
+        method: "delete"
+      })
+        .then(response => {
           return response.json();
-      }).then((data)=>{
-          if(data.ok == 1){
-              let todoIndex = $(`#${todoID}`);
-              todoIndex.html(data.value.todo);
-              resetTodosInput();
+        })
+        .then(data => {
+          if (data.ok == 1) {
+            $(`#${listItemID}`).remove();
           }
-      });
-  });
-};
+        });
+    });
+  };
 
-const deleteTodo = (todo,listItemID,deleteID)=>{
-  let deleteBtn = $(`#${deleteID}`);
-  deleteBtn.click(()=>{
-      fetch(`/${todo._id}`,{
-          method: "delete"
-      }).then((response)=>{
-          return response.json();
-      }).then((data)=>{
-          if(data.ok == 1){
-              $(`#${listItemID}`).remove();
-          }
-      });
-  });
-};
-
-  const buildIDS = (todo) => {
+  const buildIDS = todo => {
     return {
-      editID : "edit_" + todo._id,
-      deleteID : "delete_" + todo._id,
-      listItemID : "listItem_" + todo._id,
-      todoID : "todo_" + todo._id
+      editID: "edit_" + todo._id,
+      deleteID: "delete_" + todo._id,
+      listItemID: "listItem_" + todo._id,
+      todoID: "todo_" + todo._id
     };
   };
 
-  const buildTodoList = (todo, ids) => {
-    return `<li class="list-group-item id="${ids.listItemID}">
-              <div class="row">
-                <div class="col-lg-4" id="${ids.todoID}">${todo.todo}</div>
-                <div class="col-lg-4"></div>
-                <div class="col-lg-4" text-right>
-                  <button type="button"
-                          class="btn btn-secondary" 
-                          id="${ids.editID}">Edit
-                  </button>
-                  <button type="button"
-                          class="btn btn-danger" 
-                          id="${ids.deleteID}">Delete
-                  </button>
-                </div>
-              </div>
-            </li>`;
+  const buildTemplate = (todo, ids) => {
+    return `<li class="list-group-item" id="${ids.listItemID}">
+                    <div class="row">
+                        <div class="col-md-4" id="${ids.todoID}">${
+      todo.crud_todo
+    }</div>
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4 text-right">
+                            <button type="button" class="btn btn-secondary" id="${
+                              ids.editID
+                            }">Edit</button>
+                            <button type="button" class="btn btn-danger" id="${
+                              ids.deleteID
+                            }">Delete</button>
+                        </div>
+                    </div>
+               </li>`;
   };
 
-  const displayTodos = (data) => {
-    console.log(data);
-
-    data.array.forEach((todo) => {
-      let ids = buildIDS(todo) ;
-
-      display.append(buildTodoList(todo, ids));
-      editID(todo, ids.todoID, ids.editID);
-      deleteID(todo, ids.listItemID, ids.deleteID);
+  const displayTodos = data => {
+    data.forEach(crud_todo => {
+      let ids = buildIDS(crud_todo);
+      display.append(buildTemplate(crud_todo, ids));
+      editTodo(crud_todo, ids.todoID, ids.editID);
+      deleteTodo(crud_todo, ids.listItemID, ids.deleteID);
     });
   };
 
-  form.submit((e)=>{
+  form.submit(e => {
     e.preventDefault();
-    fetch('/',{
-        method : 'post',
-        body : JSON.stringify({todo : todoUserInput.val()}),
-        headers : {
-            "Content-Type" : "application/json; charset=utf-8"
-        }
-    }).then((response)=>{
+    fetch("/", {
+      method: "post",
+      body: JSON.stringify({ todo: todoUserInput.val() }),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+      .then(response => {
         return response.json();
-    }).then((data)=>{
-        if(!data.error){
-            if(data.result.ok == 1 && data.result.n == 1){
-                let ids = buildIDS(data.document);
-                display.append(buildTemplate(data.document,ids));
-                editTodo(data.document,ids.todoID,ids.editID);
-                deleteTodo(data.document,ids.listItemID,ids.deleteID);
-                displayMessage(true,data.msg);
-            }
-        }
-        else
-            displayMessage(false,data.error.message);
+      })
+      .then(data => {
+        if (!data.error) {
+          if (data.result.ok == 1 && data.result.n == 1) {
+            let ids = buildIDS(data.document);
+            display.append(buildTemplate(data.document, ids));
+            editTodo(data.document, ids.todoID, ids.editID);
+            deleteTodo(data.document, ids.listItemID, ids.deleteID);
+            displayMessage(true, data.msg);
+          }
+        } else displayMessage(false, data.error.message);
         resetTodosInput();
-    });
+      });
   });
 });
